564bba927deecd896874d5f2ab1d10368acf24fc
Ahmed Maher <ahmedsayed_g@sci.asu.edu.eg>
2024-10-26 13:27:24 +0300

    Add project files.

---
+++ Day7-Lab/Point.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7_Lab
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        
+        public Point(int x = 0, int y = 0, int z = 0)
+        {
+            X = x; 
+            Y = y; 
+            Z = z;
+        }
+
+        public Point(int x, int y) : this(x, y, 0)
+        {
+        }
+        public Point(int x) : this(x, 0, 0)
+        {
+        }
+
+        public static bool operator ==(Point a, Point b)
+            => a.Equals(b);
+        public static bool operator !=(Point a, Point b)
+            => !a.Equals(b);
+
+        public override bool Equals(object? obj)
+        {
+            Point otherPoint = obj as Point;
+            if(this.X == otherPoint.X 
+                && this.Y == otherPoint.Y
+                && this.Z == otherPoint.Z)
+                return true;
+
+            return false;
+        }
+
+        public override string ToString()
+            => $"({X}, {Y}, {Z})";
+    }
+}
---
+++ Day7-Lab/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Day7_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var P = new Point(10, 10, 10);
+            Console.WriteLine(P);
+
+            var P1 = new Point(1, 2, 3);
+
+            
+            Console.WriteLine("Enter");
+            Console.WriteLine("P && P1\n");
+            Console.WriteLine($"Equals method: {P.Equals(P1)}");
+            Console.WriteLine($"== operator: {P == P1}");
+            Console.WriteLine($"!= operator: {P != P1}");
+           
+            var P2 = new Point { X = 1, Y = 2, Z = 3 };
+
+            Console.WriteLine("P1 && P2\n");
+            Console.WriteLine($"Equals method: {P1.Equals(P2)}");
+            Console.WriteLine($"== operator: {P1 == P2}");
+            Console.WriteLine($"!= operator: {P1 != P2}");
+        }
+    }
+}
